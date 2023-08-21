d1c9458a75016608314a41c43e9782bc15560d41
Pro100Paladin <pro100paladin@icloud.com>
2023-08-21 21:50:14 +0300

    решение задачи 41

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. (Можно изучить метод Split. 
+// Пример: stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);, где первый аргумент это разделитель чисел, второе обработчик введеной последовательности)
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 4
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите через пробел все элементы: ");
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+        int count = arr.Count(x => x > 0);
+        Console.WriteLine($"Кол-во элементов больше ноля: {count}");
+    }
+}
